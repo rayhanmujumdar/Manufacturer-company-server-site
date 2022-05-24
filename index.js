171329f4ba2b02538d9d6a1a2fd0e34ee3dd6eeb
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-05-24 15:16:55 +0600

    hero ku deploy

--- index.js
+++ index.js
@@ -9,49 +9,95 @@ const jwt = require('jsonwebtoken');
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yv4lm.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// heroku deploy
+// https://fast-river-13040.herokuapp.com/ 
 
-const run = async() => {
-    try{
+const {
+    MongoClient,
+    ServerApiVersion,
+    ObjectId
+} = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yv4lm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+
+// verify token
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(' ')[1]
+    if (!token) {
+        return res.status(401).send({
+            massage: "Unauthorized"
+        })
+    }
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({
+                message: 'forbidden'
+            })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
+const run = async () => {
+    try {
         await client.connect()
         const productCollection = client.db("Manufacturer").collection('product')
         const orderCollection = client.db("Manufacturer").collection('order')
         const userCollection = client.db("Manufacturer").collection('user')
         // user collection
-        app.put('/user/:email',async(req,res) => {
+        app.put('/user/:email', async (req, res) => {
             const email = req.params.email
             const user = req.body
-            const filter = {email: email}
+            const filter = {
+                email: email
+            }
             const updateDoc = {
                 $set: user,
-              };
-            const options = { upsert: true };
+            };
+            const options = {
+                upsert: true
+            };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({email: email},process.env.TOKEN_SECRET,{
+            const token = jwt.sign({
+                email: email
+            }, process.env.TOKEN_SECRET, {
                 expiresIn: '1d'
             })
-            res.send({token,result})
+            res.send({
+                token,
+                result
+            })
         })
 
         // all product
-        app.get('/product',async (req,res) => {
+        app.get('/product', async (req, res) => {
             const query = res.query
             const result = await productCollection.find(query).toArray()
             res.send(result)
         })
         // find a single product
-        app.get('/product/:id',async(req,res) => {
-            const params = req.params.id
-            const id = {_id: ObjectId(params)}
-            const result = await productCollection.findOne(id)
-            res.send(result)
+        app.get('/product/:id', verifyToken, async (req, res) => {
+            const decoded = req.decoded.email
+            const email = req.query.email
+            if (email === decoded) {
+                const params = req.params.id
+                const id = {
+                    _id: ObjectId(params)
+                }
+                const result = await productCollection.findOne(id)
+                res.send(result)
+            }
         })
 
         // order post data
 
-        app.post('/productOrder',async(req,res) => {
+        app.post('/productOrder',verifyToken, async (req, res) => {
             const orderData = req.body
             const result = await orderCollection.insertOne(orderData)
             res.send(result)
@@ -59,11 +105,15 @@ const run = async() => {
 
         // update quantity
 
-        app.put('/product/:id',async(req,res) => {
+        app.put('/product/:id',verifyToken, async (req, res) => {
             const id = req.params.id
             const availableQuantity = req.body.quantity
-            const options = { upsert: true };
-            const filter = {_id: ObjectId(id)}
+            const options = {
+                upsert: true
+            };
+            const filter = {
+                _id: ObjectId(id)
+            }
             const updateDoc = {
                 $set: {
                     availableQuantity
@@ -72,17 +122,16 @@ const run = async() => {
             const result = await productCollection.updateOne(filter, updateDoc, options)
             res.send(result)
         })
-    }
-    finally{
+    } finally {
 
     }
 }
 run().catch(console.dir)
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('o bhaiya all is ok')
 })
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log('listening my port is ' + port)
 })
